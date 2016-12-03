51beab5f984de9a2c7a52f34ca2771ded30a4c9e
Unlogical <sakujo8744@mail.ru>
2016-12-04 00:19:02 +0300

    header_title_extractor works better

--- analizer.py
+++ analizer.py
@@ -3,47 +3,42 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 urls = [
-    # "http://habrahabr.ru/company/edison/blog/316574/",
-    # "http://geektimes.ru/post/282684/",
-    # "http://russian.rt.com/science/article/337938-mars-posadka-sssr",
-    # "http://antropogenez.ru/single-news/article/616/",
-    # "http://newtonew.com/discussions/noetic-beauty"
+    "https://habrahabr.ru/company/edison/blog/316574/",
+    "https://geektimes.ru/post/282684/",
+    "https://russian.rt.com/science/article/337938-mars-posadka-sssr",
+    "http://antropogenez.ru/single-news/article/616/",
+    "https://newtonew.com/discussions/noetic-beauty",
     "https://pypi.python.org/pypi/htmldom/2.0"
 ]
 
 
 def og_title_extractor(page):
-    print("og")
     title = page.find("meta", attrs={"property": "og:title"})
     return title["content"] if title is not None else None
 
 
 def twitter_title_extractor(page):
-    print("twitter")
     title = page.find("meta", attrs={"property": "twitter:title"})
     return title["content"] if title is not None else None
 
 
 def simple_title_extractor(page):
-    print("simple")
     title = page.title
     return title.string if title is not None else None
 
 
 def header_title_extractor(page):
-    print("header")
     title = page.h1
-    return title.text if title is not None else None
+    title = title.text.strip()
+    return title if title is not None and title != "" else None
 
 
 def meta_title_extractor(page):
-    print("meta")
     title = page.find("meta", attrs={"name": "title"})
     return title["content"] if title is not None else None
 
 
 def class_header_title_extractor(page):
-    print("class")
     title = page.find(class_ = "header")
     return title.string if title is not None else None
 
@@ -65,12 +60,16 @@ def extract_title(page, default_title):
         header_title_extractor,
         simple_title_extractor
     ]
-    return extract_feature(page, extractors, default_title)
+    clean_title = extract_feature(page, extractors, default_title).strip()
+    return clean_title.replace("\n", " ")
 
 
 for url in urls:
     print(url)
-    html = urllib.request.urlopen(url).read()
-    page = BeautifulSoup(html, 'html.parser')
+    try:
+      html = urllib.request.urlopen(url).read()
+      page = BeautifulSoup(html, 'html.parser')
+      print(extract_title(page, "unk!"))
+    except Exception as ex:
+        print("Failed to get {}, skipping".format(url))
 
-    print(extract_title(page, "unk!"))
