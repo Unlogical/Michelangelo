84f1d24669d9fbdb2ae3864515b073fccd95ee22
Unlogical <sakujo8744@mail.ru>
2016-12-03 21:59:18 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Michelangelo',
+      version='0.1',
+      description='Webpages analizer',
+      author='Unlogical',
+      author_email='sakujo8744@mail.ru',
+      packages=['beautifulsoup4'])
