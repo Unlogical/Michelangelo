ab65a96f6858596f4d456c3e920c1b4a3f93992b
Alexandra <sakujo8744@mail.ru>
2017-05-27 23:47:28 +0300

    add start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+spark-submit \
+  --master "local[2]" \
+  --name "StreamPageAnalyzer" \
+  --py-files "title_extractors.py,extraction_utils.py,content_extractors.py,author_extractors.py,date_extractor.py" \
+   run.py \
+   urls.txt \
+   out.txt
