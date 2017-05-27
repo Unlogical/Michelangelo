fc49ac770974249a5c9bee20cf493925c21fd8b9
Alexandra <sakujo8744@mail.ru>
2017-05-28 01:51:14 +0300

    add author extractors for all example pages

--- author_extractors.py
+++ author_extractors.py
@@ -18,10 +18,71 @@ def habra_author_extractor(page):
         author = author_tags
     return author
 
+def kinopoisk_author_extractor(page):
+    author_tags = page.find(class_ = "article__author_name")
+    if author_tags is not None:
+        author = author_tags.text.strip().replace("\n", " ")
+    else:
+        author = author_tags
+    return author
+
+def lenta_author_extractor(page):
+    author_tags = page.find(class_ = "name")
+    if author_tags is not None:
+        author = author_tags.text.strip().replace("\n", " ")
+    else:
+        author = author_tags
+    return author
+
+
+def vocrugsveta_author_extractor(page):
+    author_tags = page.find(class_ = "article-info__name")
+    if author_tags is not None:
+        author = author_tags.text.strip().replace("\n", " ")
+    else:
+        author = author_tags
+    return author
+
+
+def itemprop_author_extractor(page):
+    author_tags = page.find_all(itemprop = "author")
+    for tag in author_tags:
+        author = tag.text.strip().replace("\n", " ")
+        if author:
+            return author
+    return None
+
+
+def newtonew_author_extractor(page):
+    author_tags = page.find(class_ = "io-author")
+    if author_tags is not None:
+        author = author_tags.text.strip().replace("\n", " ")
+    else:
+        author = author_tags
+    return author
+
+
+def rt_author_extractor(page):
+    author_in = page.find(class_ = "article__text_article-page")
+    if author_in is None:
+        return None
+    author_tags = author_in.find('em')
+    if author_tags is None:
+        return None
+    author = author_tags.text.strip().replace("\n", " ")
+    return author
+
+
 
 def extract_author(page, default_title):
     extractors = [
         author_name_extractor,
-        habra_author_extractor
+        habra_author_extractor,
+        kinopoisk_author_extractor,
+        lenta_author_extractor,
+        vocrugsveta_author_extractor,
+        itemprop_author_extractor,
+        newtonew_author_extractor,
+        rt_author_extractor
     ]
     return extract_feature(page, extractors, default_title)
