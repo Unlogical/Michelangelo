15a7d4692d9fad56798a2e8b4719d52ef4c7edfa
Alexandra <sakujo8744@mail.ru>
2017-05-27 23:48:20 +0300

    save results to one file

--- run.py
+++ run.py
@@ -8,16 +8,18 @@ from title_extractors import extract_title
 from author_extractors import  extract_author
 from date_extractor import  extract_date
 from content_extractors import extract_content
+from pyspark.streaming.context import RDD
 
 
-if len(sys.argv) != 2:
-    print("File to urls not specified")
+if len(sys.argv) != 3:
+    print("File to urls or output file is not specified")
     exit(-1)
 
-path = sys.argv[1] # path to urls file
+path = sys.argv[1]  # path to urls file
+out_path = sys.argv[2]  # path to output file
 
 sc = SparkContext()
-ssc = StreamingContext(sc, 10)
+ssc = StreamingContext(sc, batchDuration=10)
 rdd = sc.textFile(path)
 textQueue = [rdd]
 urls = ssc.queueStream(textQueue)
@@ -39,7 +41,17 @@ def extract_data(html):
 htmls = urls.map(get_html)
 extracted_data = htmls.map(extract_data)
 # extracted_data.pprint()
-extracted_data.saveAsTextFiles("extracted_data")
+# extracted_data.saveAsTextFiles("extracted_data")
+
+
+def save_results(time, rdd: RDD):
+    if rdd.count() == 0:
+        return
+    with open(out_path, "a+") as f:
+        for item in rdd.collect():
+            print('|'.join(item), file=f)
+
+extracted_data.foreachRDD(save_results)
 
 ssc.start()
 ssc.awaitTermination()
